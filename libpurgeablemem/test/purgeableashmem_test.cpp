a8df385a7e9521cb8a18d0bd0123c36bc6ea91ef
cwx1272435 <caiyiming4@huawei.com>
2024-02-21 07:08:13 +0000

    0221
    Signed-off-by: cwx1272435 <caiyiming4@huawei.com>

--- libpurgeablemem/test/purgeableashmem_test.cpp
+++ libpurgeablemem/test/purgeableashmem_test.cpp
@@ -446,7 +446,7 @@ HWTEST_F(PurgeableAshmemTest, MutiPageReadWriteTest, TestSize.Level1)
 HWTEST_F(PurgeableAshmemTest, MutiMorePageReadWriteTest, TestSize.Level1)
 {
     size_t size = 5 * 1024 * 1024;
-    char *alphabet = (char *)malloc(size);
+    char *alphabet = static_cast<char *>(malloc(size));
     size_t len = 0;
     for (char ch = 'D'; len < size;) {
         alphabet[len++] = ch;
@@ -487,7 +487,7 @@ HWTEST_F(PurgeableAshmemTest, MutiMorePageReadWriteTest, TestSize.Level1)
 HWTEST_F(PurgeableAshmemTest, StableMutiMorePageReadWriteTest, TestSize.Level1)
 {
     size_t size = 5 * 1024 * 1024;
-    char *alphabet = (char *)malloc(size);
+    char *alphabet = static_cast<char *>(malloc(size));
     size_t len = 0;
     for (char ch = 'D'; len < size;) {
         alphabet[len++] = ch;
